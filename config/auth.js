26ff56c82c95291990315ec126de7891827e2d3e
marciano <dev@betaquark.com>
2017-05-16 12:52:23 -0300

    add

---
+++ config/auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '1912961932255661',
+		'clientSecret': '57f45857dcb2cbf8c1962abb8d07b811',
+		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+	},
+
+	'googleAuth' : {
+		'clientID': 'enter client id here',
+		'clientSecret': 'enter client secret here',
+		'callbackURL': 'enter callback here'
+	}
+}
---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+	'url'	: 'mongodb://root:vertrigo@appx-shard-00-00-gff72.mongodb.net:27017,appx-shard-00-01-gff72.mongodb.net:27017,appx-shard-00-02-gff72.mongodb.net:27017/Appx?ssl=true&replicaSet=Appx-shard-0&authSource=admin'
+}
